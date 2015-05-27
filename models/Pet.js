8bd7eb6d41659f89290ea677c4010cab313506cd
Claudia Cuevas <omordn@gmail.com>
2015-05-27 08:27:32 -0700

    add basic setup files

---
+++ models/Pet.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var petSchema = new Schema({
+  name: String,
+  owner: String,
+  weight: Number,
+  type: String
+});//end Pet Schema
+
+petSchema.methods.findSimilarTypes = function(callback) {
+  return this.model('Pet').find({type: this.type}, callback);
+};
+
+//create model
+module.exports = mongoose.model('Pet', petSchema);
