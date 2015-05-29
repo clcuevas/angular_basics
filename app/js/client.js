b31254c1119d94cfd925dd311ad49585b57a7163
Claudia Cuevas <omordn@gmail.com>
2015-05-28 17:02:31 -0700

    add angular basics with $scope

--- app/js/client.js
+++ app/js/client.js
@@ -1,20 +1,13 @@
 'use strict';
 
-var greet = require('./greet.js');
-document.write(greet());
-var petList = document.getElementById('petlist');
+require('angular/angular.js');
 
-var request = require('superagent');
+var petsApp = angular.module('petsApp', []);
 
-request
-  .get('/api/pets')
-  .end(function(err, res) {
-    if (err) return console.log(err);
-    var pets = JSON.parse(res.text);
+petsApp.controller('petsCtrl', ['$scope', function($scope) {
+  $scope.greeting = 'hello world!';
+  $scope.displayGreeting = function() {
+    alert($scope.greeting);
+  };
+}]);
 
-    pets.forEach(function(pet) {
-      var petEl = document.createElement('li');
-      petEl.innerHTML = 'Pet: ' + pet.name + ', Owner: ' + pet.owner;
-      petList.appendChild(petEl);
-    });
-  });
