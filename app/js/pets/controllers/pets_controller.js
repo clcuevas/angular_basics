a37dfb027f399a0140f1dc1fbbc2be3521f602cc
Claudia Cuevas <omordn@gmail.com>
2015-05-29 18:39:16 -0700

    add WIP cancel editing feature

--- app/js/pets/controllers/pets_controller.js
+++ app/js/pets/controllers/pets_controller.js
@@ -29,5 +29,40 @@ module.exports = function(app) {
           $scope.data.push({msg: 'could not create new note'});
         });
     };
+
+    $scope.removePet = function(pet) {
+      /*You are removing the pet element you
+      are selecting to delete (splice) and 
+      locating the index of that element
+      in your array of objects (indexOf)*/
+      $scope.pets.splice($scope.pets.indexOf(pet), 1);
+      //this deletes the object from the DB
+      $http.delete('/api/pets/' + pet._id)
+        .error(function(data) {
+          console.log(data);
+          $scope.errors.push({msg: 'could not remove note: ' + pet});
+        });
+    };
+
+    $scope.savePet = function(pet) {
+      //reset editing status
+      pet.editing = false;
+      $http.put('/api/pets/' + pet._id, pet)
+        .error(function(data) {
+          console.log(data);
+          $scope.errors.push({msg: 'could not save changes'});
+        });
+    };
+
+    $scope.cancelEditing = function() {
+      // pet.editing = false;
+      // $scope.pets.$rollbackViewValue();
+      alert('i work');
+    };
+
+    $scope.clearErrors = function() {
+      $scope.errors = [];
+      $scope.getAll();
+    };
   }]);
 };
